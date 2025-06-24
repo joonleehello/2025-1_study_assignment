a2892238d8e10bc8db2aa3915cc4ea8777423af0
joonleepc <leecpkn@naver.com>
2025-06-24 23:25:07 +0900

    6/24 fixed

--- assignment6/Run Sample 2/Assets/Scripts/GameManager.cs
+++ assignment6/Run Sample 2/Assets/Scripts/GameManager.cs
@@ -23,12 +23,18 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (Character == null) return;
+
         MoveCharacter();
     }
 
     // For smooth cam moving, it's good to use LateUpdate.
     void LateUpdate()
     {
+
+        if (Character == null) return;
+
         MoveCam();
     }
 
@@ -44,6 +50,8 @@ public class GameManager : MonoBehaviour
 
     void MoveCharacter()
     {
+
+        if (Character == null) return;
         // Character는 초당 CharacterSpeed의 속도로 우측으로 움직인다.
         // ---------- TODO ---------- 
         Character.transform.position += Vector3.right * CharacterSpeed * Time.deltaTime;
@@ -56,7 +64,7 @@ public class GameManager : MonoBehaviour
         // Character를 삭제하고, "Game Over!"라는 메시지를 3초간 띄우고, RestartButton을 활성화한다.
         // ---------- TODO ---------- 
 
-        Character.SetActive(false);
+        Destroy(Character.gameObject);
         MyUIManager.DisplayMessage("Game Over!", 3f);
         MyUIManager.RestartButton.SetActive(true);
 
